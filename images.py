a2eab2e07c2688b11e4335c798738fc3bba6d15e
DerSeb90 <stampernator@gmail.com>
2024-11-28 13:44:40 +0100

    New Blog Post on 28-11-2024 13:44:40

--- images.py
+++ images.py
@@ -4,7 +4,7 @@ import shutil
 
 # Paths (using raw strings to handle Windows backslashes correctly)
 posts_dir = r"F:\Obsidian\PrivateNotizen\posts"
-attachments_dir = r"F:\Obsidian\PrivateNotizen\attachments"
+attachments_dir = r"F:\Obsidian\PrivateNotizen\images"
 static_images_dir = r"C:\Users\Sebastian\Documents\repos\sebblog\static\images"
 
 # Step 1: Process each markdown file in the posts directory
